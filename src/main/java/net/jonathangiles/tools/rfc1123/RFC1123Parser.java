68175a273f59031f8d14571c43b10523e0c42a1f
Jonathan Giles <jonathan@jonathangiles.net>
2021-07-29 12:14:51 +1200

    Misc cleanups

--- src/main/java/net/jonathangiles/tools/rfc1123/RFC1123Parser.java
+++ src/main/java/net/jonathangiles/tools/rfc1123/RFC1123Parser.java
@@ -18,7 +18,7 @@ public class RFC1123Parser {
             parseInt(date, 17, 19),  // hour
             parseInt(date, 20, 22),  // minute
             parseInt(date, 23, 25),  // second
-            0,                                 // nanoOfSecond
+            0,                    // nanoOfSecond
             ZoneOffset.UTC);
     }
 
@@ -31,37 +31,37 @@ public class RFC1123Parser {
         return num;
     }
 
-    private static int parseMonth(final CharSequence date, final int start, final int end) {
-        final char c1 = date.charAt(start);
-        final char c2 = date.charAt(start + 1);
-        final char c3 = date.charAt(start + 2);
-
-        switch (c1) {
+    private static int parseMonth(final String date, final int start, final int end) {
+        switch (date.charAt(start)) {
             case 'J': {
                 // Jan, Jun, Jul
-                switch (c2) {
+                switch (date.charAt(start + 1)) {
                     case 'a': return 1; // Jan
                     case 'u': {
-                        switch (c3) {
+                        switch (date.charAt(start + 2)) {
                             case 'n': return 6; // Jun
                             case 'l': return 7; // Jul
+                            default: throw new IllegalArgumentException("Unknown month " + date);
                         }
                     }
+                    default: throw new IllegalArgumentException("Unknown month " + date);
                 }
             }
             case 'F': return 2; // Feb
             case 'M': {
                 // Mar, May
-                switch (c3) {
+                switch (date.charAt(start + 2)) {
                     case 'r': return 3; // Mar
                     case 'y': return 5; // May
+                    default: throw new IllegalArgumentException("Unknown month " + date);
                 }
             }
             case 'A': {
                 // Apr, Aug
-                switch (c3) {
+                switch (date.charAt(start + 2)) {
                     case 'r': return 4; // Apr
                     case 'g': return 8; // Aug
+                    default: throw new IllegalArgumentException("Unknown month " + date);
                 }
             }
             case 'S': return 9; //Sep
@@ -70,23 +70,5 @@ public class RFC1123Parser {
             case 'D': return 12; // Dec
             default: throw new IllegalArgumentException("Unknown month " + date);
         }
-
-        // The above code is quite long-winded, so a perf benchmark should be run to see if it is any better than the
-        // simpler code below
-//        switch (month) {
-//            case "Jan": return 1;
-//            case "Feb": return 2;
-//            case "Mar": return 3;
-//            case "Apr": return 4;
-//            case "May": return 5;
-//            case "Jun": return 6;
-//            case "Jul": return 7;
-//            case "Aug": return 8;
-//            case "Sep": return 9;
-//            case "Oct": return 10;
-//            case "Nov": return 11;
-//            case "Dec": return 12;
-//            default: throw new IllegalArgumentException("Unknown month " + month);
-//        }
     }
 }
